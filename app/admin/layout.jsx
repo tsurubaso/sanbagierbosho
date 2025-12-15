213f57b881d285068d8a87d73ce98a3b2fecd15f
tsurubaso <tsurubaso@hotmail.com>
2025-12-15 11:30:37 +0900

    forcing to black font in adminlayout, without changing global.css

--- app/admin/layout.jsx
+++ app/admin/layout.jsx
@@ -1,8 +1,4 @@
-import AdminLayout from "@/components/AdminLayout";
-
 import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
-
 
 import AdminSidebar from "@/components/AdminSidebar";
 import { authOptions } from "@/lib/auth"; // ✅ maintenant OK
@@ -17,7 +13,7 @@ export default async function AdminRootLayout({ children }) {
   //if (!session || session.user.email !== process.env.ADMIN_EMAIL) { redirect("/admin/login");}
 
   return (
-    <div className="flex min-h-screen">
+    <div className="flex text-black min-h-screen">
       <AdminSidebar />
       <main className="flex-1 p-6">{children}</main>
     </div>
